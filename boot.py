d31369945536aa3c6444ea59db80a376bca29d3d
Andrew St Clair <andrew_stclair95@hotmail.com>
2025-01-23 21:10:40 +1100

    refactor: rename servo to elevation for clarity in boot and main modules

--- boot.py
+++ boot.py
@@ -1,5 +1,10 @@
 """Boot function for the Antenna Rotor Controller"""
+
 from machine import Pin
+
 from lib.Servo import Servo
 
-servo = Servo(Pin(0), 10922.5, 54612.5, 180)
+
+
+elevation = Servo(Pin(0), 10922.5, 54612.5, 180)
+
--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 """Main function for the Antenna Rotor Controller"""
+
 from time import sleep
-from boot import servo
+
+from boot import elevation
 print("start")
 sleep(2)
 
+
 for i in range(0, 181, 45):
     print(i)
-    servo.angle(i)
+    elevation.angle(i)
+
     sleep(1)
 
-servo.angle(90)
+elevation.angle(90)
 sleep(2)
 
-servo.angle(0)
+elevation.angle(0)
 sleep(2)
-servo.angle(180)
+elevation.angle(180)
 sleep(2)
-servo.angle(0)
+elevation.angle(0)
 sleep(2)
-servo.angle(180)
+elevation.angle(180)
 sleep(2)
-servo.angle(90)
+elevation.angle(90)
 sleep(2)
+
 print("end")
 
-servo.close()
+elevation.close()
