6be4f922c8ddc6663746466123f2fde45e16b9a9
Andrew St Clair <andrew_stclair95@hotmail.com>
2025-01-23 21:27:06 +1100

    feat: initialize UART for command input and add .gitignore and pylintrc configuration

--- boot.py
+++ boot.py
@@ -1,10 +1,15 @@
 """Boot function for the Antenna Rotor Controller"""
 
 from machine import Pin
+from machine import UART
 
 from lib.Servo import Servo
 
+# Initialize the USB Serial device for commands
+uart = UART(0, 115200)
+uart.init(115200, bits=8, parity=None, stop=1)
+uart.write("UART Initialized\n")
 
-
+# Initialize the elevation servo
 elevation = Servo(Pin(0), 10922.5, 54612.5, 180)
 
